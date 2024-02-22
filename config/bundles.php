7a5045c1f4c84066322e3944a3b372a405b58b9f
Yoann Kergall <yoann.kergall@gmail.com>
2024-02-23 00:00:13 +0100

    remove framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
